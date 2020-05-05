26064340592c75ce49e6f1b32a69daf707c89721
okobz <okaibedi@gmail.com>
2020-05-05 18:06:21 +0100

    change to gatsby config; "Strong People of React"

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi folks</h1>
+    <p>Welcome to my first React App</p>
+    <p>I'm something great at a world class level.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
